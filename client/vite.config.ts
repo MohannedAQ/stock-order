d6b8d572a65dbed6dbaba18a072f99592901fb28
MohannedAQ <93926106+MohannedAQ@users.noreply.github.com>
2025-12-06 04:15:41 +0000

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -25,9 +25,9 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": path.resolve(import.meta.dirname, "client", "src"),
-      "@shared": path.resolve(import.meta.dirname, "shared"),
-      "@assets": path.resolve(import.meta.dirname, "attached_assets"),
+  "@": "/src",
+  "@shared": "/shared",
+  "@assets": "/attached_assets",
     },
   },
   css: {
@@ -35,9 +35,9 @@ export default defineConfig({
       plugins: [],
     },
   },
-  root: path.resolve(import.meta.dirname, "client"),
+  root: import.meta.dirname
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    outDir: "dist"
     emptyOutDir: true,
   },
   server: {
