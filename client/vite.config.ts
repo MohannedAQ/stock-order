55772874d155ed5e2829d4a401ad13ceac2c272b
MohannedAQ <93926106+MohannedAQ@users.noreply.github.com>
2025-12-06 06:29:28 +0000

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,18 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import path from "path"
 
 export default defineConfig({
   plugins: [react()],
-  root: path.resolve(__dirname),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
-      "@assets": path.resolve(__dirname, "attached_assets") // <--- THIS IS CORRECT
-    }
+    },
   },
+  publicDir: "attached_assets",
   build: {
     outDir: "dist",
-    emptyOutDir: true
-  }
-});
+    emptyOutDir: true,
+  },
+})
