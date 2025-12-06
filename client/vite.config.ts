bb2dd0a409897f70344ad742dd83239bfd10b084
MohannedAQ <93926106+MohannedAQ@users.noreply.github.com>
2025-12-06 05:27:07 +0000

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
-  root: path.resolve(__dirname, "client"),
+  plugins: [react()],
+  root: ".",
   build: {
-    outDir: path.resolve(__dirname, "dist/public"),
-    emptyOutDir: true
-  }
+    outDir: "dist",
+    emptyOutDir: true,
+  },
 });
